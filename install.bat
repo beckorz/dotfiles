c8ec114fb3ddf5a44886cdbf0c43256415c3dd77
beckorz <beckorz+github@gmail.com>
2020-03-12 13:59:55 +0900

    ✨[Add] gitのコミット時のテンプレートファイルを追加

--- install.bat
+++ install.bat
@@ -9,15 +9,16 @@ pause
 SET DIR_HOME=%USERPROFILE%
 SET DIR_DOTFILES=%USERPROFILE%\dotfiles
 
-mklink    %DIR_HOME%\.ctags     %DIR_DOTFILES%\.ctags
-mklink    %DIR_HOME%\.gitignore %DIR_DOTFILES%\.gitignore
-mklink    %DIR_HOME%\.gitconfig %DIR_DOTFILES%\.gitconfig
-mklink    %DIR_HOME%\.jshintrc  %DIR_DOTFILES%\.jshintrc
-mklink    %DIR_HOME%\.minttyrc  %DIR_DOTFILES%\.minttyrc
-mklink    %DIR_HOME%\.vimrc     %DIR_DOTFILES%\.vimrc
-mklink    %DIR_HOME%\.gvimrc    %DIR_DOTFILES%\.gvimrc
-mklink    %DIR_HOME%\init.cmd   %DIR_DOTFILES%\init.cmd
-mklink /D %DIR_HOME%\vimfiles   %DIR_DOTFILES%\vimfiles
+mklink    %DIR_HOME%\.ctags           %DIR_DOTFILES%\.ctags
+mklink    %DIR_HOME%\.gitignore       %DIR_DOTFILES%\.gitignore
+mklink    %DIR_HOME%\.gitconfig       %DIR_DOTFILES%\.gitconfig
+mklink    %DIR_HOME%\.jshintrc        %DIR_DOTFILES%\.jshintrc
+mklink    %DIR_HOME%\.minttyrc        %DIR_DOTFILES%\.minttyrc
+mklink    %DIR_HOME%\.vimrc           %DIR_DOTFILES%\.vimrc
+mklink    %DIR_HOME%\.gvimrc          %DIR_DOTFILES%\.gvimrc
+mklink    %DIR_HOME%\.commit_template %DIR_DOTFILES%\.commit_template
+mklink    %DIR_HOME%\init.cmd         %DIR_DOTFILES%\init.cmd
+mklink /D %DIR_HOME%\vimfiles         %DIR_DOTFILES%\vimfiles
 
 mkdir %DIR_HOME%\vimfiles\tmp
 mkdir %DIR_HOME%\vimfiles\undo
--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 export DIR_HOME=~
 export DIR_DOTFILES=~/dotfiles
 
-ln -sf $DIR_DOTFILES/.gitignore $DIR_HOME/.gitignore
-ln -sf $DIR_DOTFILES/.gitconfig $DIR_HOME/.gitconfig
-ln -sf $DIR_DOTFILES/.jshintrc  $DIR_HOME/.jshintrc
-ln -sf $DIR_DOTFILES/.minttyrc  $DIR_HOME/.minttyrc
-ln -sf $DIR_DOTFILES/.vimrc     $DIR_HOME/.vimrc
-ln -sf $DIR_DOTFILES/.gvimrc    $DIR_HOME/.gvimrc
-ln -sf $DIR_DOTFILES/init.cmd   $DIR_HOME/init.cmd
-ln -sf $DIR_DOTFILES/vimfiles   $DIR_HOME/vimfiles
+ln -sf $DIR_DOTFILES/.gitignore         $DIR_HOME/.gitignore
+ln -sf $DIR_DOTFILES/.gitconfig         $DIR_HOME/.gitconfig
+ln -sf $DIR_DOTFILES/.jshintrc          $DIR_HOME/.jshintrc
+ln -sf $DIR_DOTFILES/.minttyrc          $DIR_HOME/.minttyrc
+ln -sf $DIR_DOTFILES/.vimrc             $DIR_HOME/.vimrc
+ln -sf $DIR_DOTFILES/.gvimrc            $DIR_HOME/.gvimrc
+ln -sf $DIR_DOTFILES/init.cmd           $DIR_HOME/init.cmd
+ln -sf $DIR_DOTFILES/vimfiles           $DIR_HOME/vimfiles
+ln -sf $DIR_DOTFILES/.commit_template   $DIR_HOME/.commit_template
 
